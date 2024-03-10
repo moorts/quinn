4ad4b5c05dc21017784a29a9a23a651648837077
moorts <moritz.gutfleisch@gmx.de>
2024-03-10 15:32:47 +0100

    Change MAX_CID_LEN

--- quinn-proto/src/lib.rs
+++ quinn-proto/src/lib.rs
@@ -297,7 +297,7 @@ pub struct Transmit {
 /// The maximum number of CIDs we bother to issue per connection
 const LOC_CID_COUNT: u64 = 8;
 const RESET_TOKEN_SIZE: usize = 16;
-const MAX_CID_SIZE: usize = 20;
+const MAX_CID_SIZE: usize = 255;
 const MIN_INITIAL_SIZE: u16 = 1200;
 /// <https://www.rfc-editor.org/rfc/rfc9000.html#name-datagram-size>
 const INITIAL_MTU: u16 = 1200;
